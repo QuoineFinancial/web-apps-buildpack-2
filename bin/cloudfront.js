b841640a61520d3d190e310dd2cae8432d8cb18d
Thien Do <dvkndn@gmail.com>
2017-09-25 12:22:30 +0900

    Switch to S3 and CF (#2)
    * Make the build message more beautiful 🌸
    
    It will look like http://imgur.com/a/x9oiX
    
    * Build all apps
    
    * Fix if statement
    
    * Fix -eq need int not string
    
    * Fix copy
    
    * Fix prefix
    
    * Build 4 apps only
    
    * Update compile
    
    * Update compile
    
    * Update compile
    
    * Update compile
    
    * Update compile
    
    * Update compile
    
    * Update compile
    
    * Update compile
    
    * Update compile
    
    * Update compile
    
    * Echo REACT_APP_APP instead of iapp
    
    * Add JS build script
    
    * Fix node build
    
    * Fix build
    
    * Add install
    
    * Update
    
    * Update install
    
    * Test
    
    * Fix install
    
    * Revert
    
    * Use NodeJS to invalidate CloudFront
    
    * Update
    
    * Update
    
    * Debug
    
    * Update CloudFront
    
    * Fix again
    
    * Fix omg...
    
    * export REACT_APP_APP
    
    * Update scripts
    
    * Fix CloudFront
    
    * Update CloudFront message
    
    * Update buildpack message
    
    * Update build message
    
    * unzip -qq
    
    * Update messge
    
    * split global dependencies from core to root
    
    * Update build message for S3 + CloudFront
    
    * without quite
    
    * error and warning only
    
    * size only
    
    * Prefix S3 with 6 spaces
    
    * summary invalidate output
    
    * unified build output
    
    * force index html hopefully
    
    * force html index
    
    * fix
    
    * show error and warning only

---
+++ bin/cloudfront.js
@@ -0,0 +1,29 @@
+// This will invalidate CloudFront based on S3
+// The app need to know the AWS_S3_BUCKET
+// Example: node cloudfront.js quoinex-stag9
+
+const { exec, execSync } = require('child_process');
+const AWS_S3_BUCKET = process.env.AWS_S3_BUCKET;
+const BUILD_DIR = process.env.BUILD_DIR;
+
+exec(`${BUILD_DIR}/bin/aws cloudfront list-distributions`, (err, stdout, stderr) => {
+  if (err) {
+    return console.log(stderr);
+  }
+
+  const distributions = JSON.parse(stdout).DistributionList.Items;
+  const matchedS3Bucket = name => (name.indexOf(AWS_S3_BUCKET) > -1);
+
+  distributions
+    .filter(distribution => matchedS3Bucket(distribution.DefaultCacheBehavior.TargetOriginId))
+    .forEach((distribution, index, all) => {
+      try {
+        const cname = distribution.Aliases.Items[0]
+        console.log(`       [${index + 1}/${all.length}] ${cname}...`);
+      } catch (error) {
+        console.log(`       Warning: the distribution ${distribution.Id} does not have CNAME value`);
+      }
+      execSync(`${BUILD_DIR}/bin/aws cloudfront create-invalidation --distribution-id ${distribution.Id} --paths "/*"`)
+    });
+});
+
