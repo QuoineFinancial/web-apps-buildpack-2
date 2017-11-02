eb5e3a7a7a82f5c0fd59bf567fc38d7629ac412a
Đăng Nguyễn <nguyenhuedang@gmail.com>
2017-11-02 11:37:32 +0700

    Add chat and lend app

--- bin/utils/config.js
+++ bin/utils/config.js
@@ -1,11 +1,13 @@
 module.exports =
   {
-    apps: ['accounts', 'trade', 'home', 'chart', 'tokens'],
+    apps: ['accounts', 'trade', 'home', 'chart', 'tokens', 'chat', 'lend'],
     appUrlKeys: {
       'accounts': 'REACT_APP_ACCOUNTS_HOST',
       'trade': 'REACT_APP_TRADE_HOST',
       'home': 'REACT_APP_HOME_HOST',
       'chart': 'REACT_APP_CHART_HOST',
       'tokens': 'REACT_APP_TOKENS_HOST',
+      'chat': 'REACT_APP_CHAT_HOST',
+      'lend': 'REACT_APP_LEND_HOST',
     }
   }
