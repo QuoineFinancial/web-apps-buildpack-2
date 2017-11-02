c4e538491cf754c8f898a7e3442643a2b0bcbb25
Đăng Nguyễn <nguyenhuedang@gmail.com>
2017-11-02 13:39:33 +0700

    Remove lend

--- bin/utils/config.js
+++ bin/utils/config.js
@@ -1,6 +1,6 @@
 module.exports =
   {
-    apps: ['accounts', 'trade', 'home', 'chart', 'tokens', 'chat', 'lend'],
+    apps: ['accounts', 'trade', 'home', 'chart', 'tokens', 'chat'],
     appUrlKeys: {
       'accounts': 'REACT_APP_ACCOUNTS_HOST',
       'trade': 'REACT_APP_TRADE_HOST',
@@ -8,6 +8,5 @@ module.exports =
       'chart': 'REACT_APP_CHART_HOST',
       'tokens': 'REACT_APP_TOKENS_HOST',
       'chat': 'REACT_APP_CHAT_HOST',
-      'lend': 'REACT_APP_LEND_HOST',
     }
   }
