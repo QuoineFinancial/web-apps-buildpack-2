67ceea286d85775ada462f5b38c6a2a61a417744
Huong Hk <huong.quoine@gmail.com>
2017-11-14 11:28:42 +0700

    Add lend app

--- bin/utils/config.js
+++ bin/utils/config.js
@@ -1,11 +1,12 @@
 module.exports =
   {
-    apps: ['accounts', 'trade', 'home', 'chart', 'tokens'],
+    apps: ['accounts', 'trade', 'home', 'chart', 'tokens', 'lend'],
     appUrlKeys: { 
       'accounts': 'REACT_APP_ACCOUNTS_HOST',
       'trade': 'REACT_APP_TRADE_HOST',
       'home': 'REACT_APP_HOME_HOST',
       'chart': 'REACT_APP_CHART_HOST',
       'tokens': 'REACT_APP_TOKENS_HOST',
+      'lend': 'REACT_APP_LEND_HOST',
     }
   }
